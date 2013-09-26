5ea588361bb9c3610decd120bab9510b6b4c4fd9
Peter Göbel <info@gopeter.de>
2013-09-26 17:08:29 +0200

    fixed rotation calc

--- client/js/lib.js
+++ client/js/lib.js
@@ -165,11 +165,11 @@ function checkCursorPosition() {
      */    
     
     if (direction == 'left') {
-        cursor_position_in_percent = 1 - (cursor_x / navigation_left_width);
-        rotation = rotation + Math.pow((0.012 * cursor_position_in_percent), 4);
+        cursor_position_in_percent = (1 - (cursor_x / navigation_left_width)) * 100;
+        rotation = rotation + Math.pow((0.012 * cursor_position_in_percent), 5);
     } else if (direction == 'right') {
-        cursor_position_in_percent = 1 - (((window_width - cursor_x) / (window_width - navigation_right_width)));
-        rotation = rotation - Math.pow((0.012 * cursor_position_in_percent), 4);
+        cursor_position_in_percent = (1 - (((window_width - cursor_x) / (window_width - navigation_right_width)))) * 100;
+        rotation = rotation - Math.pow((0.012 * cursor_position_in_percent), 5);
     }
     
     if (rotation < 0) {
